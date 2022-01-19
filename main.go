28b9607816f675e111ef8c16671a1a1321b053c5
Anshul Goyal <anshul.goyal@cred.club>
2022-01-20 02:24:42 +0530

    fix

--- main.go
+++ main.go
@@ -7,20 +7,20 @@ import (
 	"net"
 
 	// importing generated stubs
-	grpc_gateway_demo "grpc-gateway-demo/gen/go/hello"
+	gen "grpc-gateway-demo/gen/go/hello"
 
 	"google.golang.org/grpc"
 )
 
 // GreeterServerImpl will implement the service defined in protocol buffer definitions
 type GreeterServerImpl struct {
-	grpc_gateway_demo.UnimplementedGreeterServer
+	gen.UnimplementedGreeterServer
 }
 
 // SayHello is the implementation of RPC call defined in protocol definitions.
 // This will take HelloRequest message and return HelloReply
-func (g *GreeterServerImpl) SayHello(ctx context.Context, request *grpc_gateway_demo.HelloRequest) (*grpc_gateway_demo.HelloReply, error) {
-	return &grpc_gateway_demo.HelloReply{
+func (g *GreeterServerImpl) SayHello(ctx context.Context, request *gen.HelloRequest) (*gen.HelloReply, error) {
+	return &gen.HelloReply{
 		Message: fmt.Sprintf("hello %s",request.Name),
 	},nil
 }
@@ -29,7 +29,7 @@ func main() {
 	// create new gRPC server
 	server := grpc.NewServer()
 	// register the GreeterServerImpl on the gRPC server
-	grpc_gateway_demo.RegisterGreeterServer(server, &GreeterServerImpl{})
+	gen.RegisterGreeterServer(server, &GreeterServerImpl{})
 	// start listening on port :8080 for a tcp connection
 	if l, err := net.Listen("tcp", ":8080"); err != nil {
 		log.Fatal("error in listening on port :8080", err)
