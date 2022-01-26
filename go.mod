bdea36a29e381067270a3c8c7215d47a59f6bdf1
Anshul Goyal <anshul.goyal@cred.club>
2022-01-27 04:05:16 +0530

    add gin

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/envoyproxy/protoc-gen-validate v0.6.3 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2
 	google.golang.org/genproto v0.0.0-20220107163113-42d7afdf6368
 	google.golang.org/grpc v1.43.0
---
+++ proxy_gin/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	gen "grpc-gateway-demo/gen/go/hello"
+)
+
+var allowedHeaders=map[string]struct{}{
+	"x-request-id": {},
+}
+
+func isHeaderAllowed(s string)( string,bool) {
+	// check if allowedHeaders contain the header
+	if _,isAllowed:=allowedHeaders[s];isAllowed {
+		// send uppercase header
+		return strings.ToUpper(s),true
+	}
+	// if not in allowed header, don't send the header
+	return s,false
+}
+
+func main() {
+	// creating mux for gRPC gateway. This will multiplex or route request different gRPC service
+	mux:=runtime.NewServeMux(
+		// convert header in response(going from gateway) from metadata received.
+		runtime.WithOutgoingHeaderMatcher(isHeaderAllowed),
+		runtime.WithMetadata(func(ctx context.Context, request *http.Request) metadata.MD {
+			header:=request.Header.Get("Authorization")
+			// send all the headers received from the client
+			md:=metadata.Pairs("auth",header)
+			return md
+		}),
+		runtime.WithErrorHandler(func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, writer http.ResponseWriter, request *http.Request, err error) {
+			//creating a new HTTTPStatusError with a custom status, and passing error
+			newError:=runtime.HTTPStatusError{
+				HTTPStatus: 400,
+				Err:        err,
+			}
+			// using default handler to do the rest of heavy lifting of marshaling error and adding headers
+			runtime.DefaultHTTPErrorHandler(ctx,mux,marshaler,writer,request,&newError)
+		}))
+	// setting up a dail up for gRPC service by specifying endpoint/target url
+	err := gen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, "localhost:8080", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Creating a normal HTTP server
+	server:=gin.New()
+	server.Use(gin.Logger())
+	server.Any("v1/*{grpc_gateway}",gin.WrapH(mux))
+
+	// start server
+	err = server.Run(":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
