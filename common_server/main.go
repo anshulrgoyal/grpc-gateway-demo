59a378624e6c62f7b40e59c802b43d7233e419a6
Anshul Goyal <anshul.goyal@cred.club>
2022-01-27 04:33:24 +0530

    remove junk

--- common_server/main.go
+++ common_server/main.go
@@ -6,13 +6,10 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"strings"
 
 	"github.com/felixge/httpsnoop"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/soheilhy/cmux"
-	"google.golang.org/grpc/metadata"
-
 	// importing generated stubs
 	gen "grpc-gateway-demo/gen/go/hello"
 
@@ -41,24 +38,7 @@ func main() {
 	// register the GreeterServerImpl on the gRPC server
 	gen.RegisterGreeterServer(grpcSever, &GreeterServerImpl{})
 	// creating mux for gRPC gateway. This will multiplex or route request different gRPC service
-	mux:=runtime.NewServeMux(
-		// convert header in response(going from gateway) from metadata received.
-		runtime.WithOutgoingHeaderMatcher(isHeaderAllowed),
-		runtime.WithMetadata(func(ctx context.Context, request *http.Request) metadata.MD {
-			header:=request.Header.Get("Authorization")
-			// send all the headers received from the client
-			md:=metadata.Pairs("auth",header)
-			return md
-		}),
-		runtime.WithErrorHandler(func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, writer http.ResponseWriter, request *http.Request, err error) {
-			//creating a new HTTTPStatusError with a custom status, and passing error
-			newError:=runtime.HTTPStatusError{
-				HTTPStatus: 400,
-				Err:        err,
-			}
-			// using default handler to do the rest of heavy lifting of marshaling error and adding headers
-			runtime.DefaultHTTPErrorHandler(ctx,mux,marshaler,writer,request,&newError)
-		}))
+	mux:=runtime.NewServeMux()
 	// setting up a dail up for gRPC service by specifying endpoint/target url
 	err := gen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, "localhost:8081", []grpc.DialOption{grpc.WithInsecure()})
 	if err != nil {
@@ -88,18 +68,4 @@ func withLogger(handler http.Handler) http.Handler {
 		m:=httpsnoop.CaptureMetrics(handler,writer,request)
 		log.Printf("http[%d]-- %s -- %s\n",m.Code,m.Duration,request.URL.Path)
 	})
-}
-
-var allowedHeaders=map[string]struct{}{
-	"x-request-id": {},
-}
-
-func isHeaderAllowed(s string)( string,bool) {
-	// check if allowedHeaders contain the header
-	if _,isAllowed:=allowedHeaders[s];isAllowed {
-		// send uppercase header
-		return strings.ToUpper(s),true
-	}
-	// if not in allowed header, don't send the header
-	return s,false
 }
